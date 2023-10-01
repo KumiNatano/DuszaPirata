4413f2f4786f1bdbc7abfe09b2d5869d09c7704c
kacper119p <kacper119p@gmail.com>
2023-10-01 22:57:52 +0200

    Initial Commit

---
+++ Source/DuszaPirata/DuszaPirataGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DuszaPirataGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADuszaPirataGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADuszaPirataGameMode();
+};
+
+
+
