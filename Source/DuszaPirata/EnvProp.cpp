3d53dcd1f0e9bd7afaffcc33f6cfef728dc0c0df
Wiktor Czernik <git@czernik.dev>
2023-10-04 21:02:15 +0200

    Created generic EnvProp class, Test Barrel blueprint and added those to the map

---
+++ Source/DuszaPirata/EnvProp.cpp
@@ -0,0 +1,19 @@
+#include "EnvProp.h"
+
+AEnvProp::AEnvProp()
+{
+	//PrimaryActorTick.bCaWnEverTick = true;
+}
+
+void AEnvProp::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+/*
+void AEnvProp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+*/
