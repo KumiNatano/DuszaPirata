06709118469495c57d65a2270875813a8a461536
DevJacek112 <jacekteodorczyk01@gmail.com>
2023-10-17 00:04:42 +0200

    Finally commiting cpp HP system.

---
+++ Source/DuszaPirata/Public/HealthSystem.h
@@ -0,0 +1,37 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthSystem.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DUSZAPIRATA_API UHealthSystem : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthSystem();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float DefaultHealth;
+	
+	UPROPERTY(BlueprintReadWrite)
+	float Health;
+
+	UPROPERTY(BlueprintReadWrite)
+	float MaxHealth;
+
+	UFUNCTION()
+	void TakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+	UFUNCTION()
+	void die();
+};
