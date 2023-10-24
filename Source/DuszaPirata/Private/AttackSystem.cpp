ad2f7fc833a7b4d80feb138d0c0da7f1fc8dcc94
Diofantos232 <jakub.g.232@gmail.com>
2023-10-24 15:59:59 +0200

    Added AttackSystem class
    (Still work in progress)

---
+++ Source/DuszaPirata/Private/AttackSystem.cpp
@@ -0,0 +1,35 @@
+// 
+
+
+#include "AttackSystem.h"
+
+#include "HealthSystem.h"
+
+
+// Sets default values for this component's properties
+UAttackSystem::UAttackSystem()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	DefaultDamage = 50;
+	Damage = DefaultDamage;
+}
+
+
+// Called when the game starts
+void UAttackSystem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+void UAttackSystem::DealDamage(AActor* DamagedActor, const class UDamageType* DamageType,
+	class AController* InstigatedBy, AActor* DamageCauser)
+{
+	
+}
+
