62764577f5d6dc638d71a55214f847f20e113218
Polybro <viktor.chernikov@onet.pl>
2024-02-13 22:19:17 +0100

    Base class for statuses that cause some damage to the health of entities

---
+++ Source/DuszaPirata/Public/Statuses/EntityDamageStatus.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EntityStatus.h"
+#include "HealthSystem.h"
+#include "EntityDamageStatus.generated.h"
+
+
+UENUM(BlueprintType)
+enum EEntityStatusTickDamageType
+{
+	Relative,  // Damage is given relative to delta time, lifespan of status and total damage.
+	Constant   // Invoke tick event every x seconds
+};
+
+UCLASS()
+class DUSZAPIRATA_API UEntityDamageStatus : public UEntityStatus
+{
+	GENERATED_BODY()
+protected:
+	virtual void ActorAssigned_Implementation(AActor* Actor) override;
+	virtual void Started_Implementation() override;
+	virtual void Ticked_Implementation(float DeltaTime) override;
+	virtual void Ended_Implementation() override;
+public:
+	/**
+	 * Tells if status should damage actor's health on start.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Settings")
+	bool DamageOnStart = false;
+	
+	/**
+	 * Tells if status should damage actor's health each tick.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Settings")
+	bool DamageOnTick = false;
+	
+	/**
+	 * Tells if status should damage actor's health each tick.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Settings")
+	bool DamageOnEnd = false;
+	
+	/**
+	 * Damage given to actor's health on start.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Settings")
+	float StartDamage = 0;
+	
+	/**
+	 * Total damage given to actor's health during life of status.
+	 * Used when tick damage type is set to Relative.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Settings")
+	float TotalDamage = 0;
+	
+	/**
+	 * Damage given to actor's health each tick.
+	 * Used when tick damage type is set to Constant.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Settings")
+	float TickDamage = 0;
+	
+	/**
+	 * Damage given to actor's health on end.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Settings")
+	float EndDamage = 0;
+
+	/**
+	 * Determines how much damage should be given to actor's health each tick.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Settings")
+	TEnumAsByte<EEntityStatusTickDamageType> TickDamageType;
+
+	
+	/**
+	 * Damage given to actor's health during work.
+	 */
+	UPROPERTY(BlueprintReadOnly, Category="State")
+	float DamageGiven = 0;
+	
+	/**
+	 * A health component of target actor.
+	 */
+	UPROPERTY(BlueprintReadOnly, Category="State")
+	UHealthSystem* Health;
+};
